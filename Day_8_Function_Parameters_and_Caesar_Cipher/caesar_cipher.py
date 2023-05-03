6f612a554f4fbce834834f7c471c4a7988f8d6c0
Jers1e <128065477+Jers1e@users.noreply.github.com>
2023-05-03 20:53:16 +0000

    Merged encrypt and decrypt functions into one caesar function, added additional comments

--- Day_8_Function_Parameters_and_Caesar_Cipher/caesar_cipher.py
+++ Day_8_Function_Parameters_and_Caesar_Cipher/caesar_cipher.py
@@ -1,41 +1,43 @@
+#Letter database in which the caesar function will be pulling from
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
             'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
+
+#Asks input questions to get the arguments for caesar function
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-
-def encrypt(plain_text, shift_amount):
-    cipher = ""
-    for letter in plain_text:
-        position = alphabet.index(letter)
-        new_position = position + shift_amount
-        if new_position > 25:
-            # This code prevents the glitching of if the input extends beyond the original alphabet list, if it does, it creates a new list that doubles the original one,
-            # essentially making it repeat, if it doesn't go over however, it continues the decryption within the original list
-            extended_alphabet = alphabet + alphabet
-            position = extended_alphabet.index(letter)
-            new_position = position + shift_amount
-            new_letter = extended_alphabet[new_position]
-            cipher += new_letter
-        else:
+#* Caesar Function
+def caesar(text, shift, direction):
+#Encryption
+    if direction == "encode":
+        cipher = ""
+        for letter in text:
+            position = alphabet.index(letter)
+            new_position = position + shift
+            if new_position > 25:
+                # This code prevents the glitching of if the input extends beyond the original alphabet list, if it does, it creates a new list that doubles the original one,
+                # essentially making it repeat, if it doesn't go over however, it continues the decryption within the original list
+                extended_alphabet = alphabet + alphabet
+                position = extended_alphabet.index(letter)
+                new_position = position + shift_amount
+                new_letter = extended_alphabet[new_position]
+                cipher += new_letter
+            else:
+                new_letter = alphabet[new_position]
+                cipher += new_letter
+        print(f"The encoded text is {cipher}")
+#Decryption
+    elif direction == "decode":
+        d_cipher = ""
+        for letter in text:
+            position = alphabet.index(letter)
+            new_position = position - shift
             new_letter = alphabet[new_position]
-            cipher += new_letter
-    print(f"The encoded text is {cipher}")
+            d_cipher += new_letter
+        print(f"The decoded text is {d_cipher}")
+    else:
+        print("Invalid direction.")
 
-
-def decrypt(cipher_text, shift_amount):
-    d_cipher = ""
-    for letter in cipher_text:
-        position = alphabet.index(letter)
-        new_position = position - shift_amount
-        new_letter = alphabet[new_position]
-        d_cipher += new_letter
-    print(f"The decoded text is {d_cipher}")
-
-
-if direction == 'encode':
-    encrypt(plain_text=text, shift_amount=shift)
-elif direction == 'decode':
-    decrypt(cipher_text=text, shift_amount=shift)
+caesar(text, shift, direction)
